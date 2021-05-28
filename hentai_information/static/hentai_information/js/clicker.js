748c2261fae1356acb280769207f8159be7e99c7
Ivan <chek539camo@gmail.com>
2021-05-28 12:04:24 +0500

    Added sound-1.mp3 and sound-2.mp3 for boosts. Added list of boostSounds at clicker.js. Added argument of forloop counter0 to buyBoost at clicker_page.html. Change height size of main content for 90vh, deleted grid-row in classes at style.css.

--- hentai_information/static/hentai_information/js/clicker.js
+++ hentai_information/static/hentai_information/js/clicker.js
@@ -1,18 +1,35 @@
 let textClickPower = 'Your click power: ';
 let textCoins = 'You have: ';
 let textPrice = ' coins';
+let sound = new Audio();
+sound.src = 'static/hentai_information/audio/nya.mp3'
+sound.volume = 0.08;
+let boostSounds = []
+boostsCount = 2;
+for (let i = 1; i < boostsCount + 1; i++) {
+    let newSound = new Audio();
+    newSound.src = 'static/hentai_information/audio/sound-' + i + '.mp3';
+    boostSounds.push(newSound);
+    boostSounds[i - 1].volume = 0.4;
+}
 
 
 async function callbackClick() {
-    let sound = new Audio();
-    sound.src = 'static/hentai_information/audio/nya.mp3'
-    sound.volume = 0.1;
-    await sound.play();
+
+    if (sound.currentTime > 0.3 || sound.currentTime === 0) {
+        sound.pause();
+        sound.currentTime = 0;
+        await sound.play();
+    }
+
     let coins = await (await fetch('/clicker/click/', {method: 'GET'})).json();
     document.querySelector('.coins').innerHTML = textCoins + coins
 }
 
-async function buyBoost(id) {
+async function buyBoost(id, count) {
+    boostSounds[count].pause();
+    boostSounds[count].currentTime = 0;
+    await boostSounds[count].play();
     let data = await (await fetch('/clicker/buyBoost/' + id + '/', {method: 'GET'})).json();
     document.querySelector('.clickPower').innerHTML = textClickPower + data['clickPower'];
     document.querySelector('.coins').innerHTML = textCoins + data['coins'];
