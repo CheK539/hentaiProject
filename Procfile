fc7c31b40fd27520dcdd85b28feb3ce8428f5826
Ivan <chek539camo@gmail.com>
2021-06-09 20:49:33 +0500

    Created requirements.txt, runtime.txt, Procfile, Procfile.windows and added heroku settings at settings.py.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hentaiProject.wsgi
--- hentaiProject/settings.py
+++ hentaiProject/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +28,6 @@ SECRET_KEY = 'a*@+=q##ee3z$muuddw$mi3-x47ed+t^cz3(1vas-8e$r+*s(9'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hentaiProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -126,3 +123,5 @@ LOGIN_REDIRECT_URL = 'hentai_information:clickerPage'
 LOGOUT_REDIRECT_URL = 'authentication:login'
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
