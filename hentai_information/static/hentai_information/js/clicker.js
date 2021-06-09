b0f4954a7f08a9d338581266c6414e23561e32b3
Ivan <chek539camo@gmail.com>
2021-06-09 17:29:50 +0500

    Added image autoBoost-1.png. Added auto click block, text block for instead h2 tags, changed boost-template at clicker_page.html. Created activateBoosts, setAutoClickInterval, saveCoinsInterval, getCache functions, refactored codes at clicker.js. Added new css rules at style.css.

--- hentai_information/static/hentai_information/js/clicker.js
+++ hentai_information/static/hentai_information/js/clicker.js
@@ -1,11 +1,14 @@
-let textClickPower = 'Your click power: ';
-let textCoins = 'You have: ';
-let textPrice = ' coins';
 let sound = new Audio();
 sound.src = 'static/hentai_information/audio/nya.mp3'
 sound.volume = 0.08;
 let boostSounds = []
 boostsCount = 2;
+let userId = JSON.parse(document.getElementById('user_id').textContent);
+let cache = getCache()
+
+activateBoosts()
+setAutoClickInterval()
+saveCoinsInterval()
 
 for (let i = 1; i < boostsCount + 1; i++) {
     let newSound = new Audio();
@@ -14,46 +17,59 @@ for (let i = 1; i < boostsCount + 1; i++) {
     boostSounds[i - 1].volume = 0.4;
 }
 
+async function getCache() {
+    return (await (await fetch('/json/' + userId, {method: 'GET'})).json())[0]
+}
 
 async function callbackClick() {
 
     if (sound.currentTime > 0.3 || sound.currentTime === 0) {
-        sound.pause();
+        await sound.pause();
         sound.currentTime = 0;
         await sound.play();
     }
 
-    let data = await (await fetch('/clicker/click/', {method: 'GET'})).json();
+    cache.then(data => {
+        data['coins'] += data['clickPower']
 
-    document.querySelector('.coins').innerHTML = textCoins + data['coins']
-
-    let boost = data['boost']
-
-    if (boost)
-        renderBoost(boost)
+        document.querySelector('.coins .data').innerText = data['coins']
+        activateBoosts()
+    })
 }
 
 function buyBoost(id, count) {
-    let token = getCookie('csrftoken')
-
     boostSounds[count].pause();
     boostSounds[count].currentTime = 0;
     boostSounds[count].play();
 
-    fetch('/clicker/buyBoost/', {
-        method: 'POST',
-        headers: {"X-CSRFToken": token, 'Content-Type': 'application/json'},
-        body: JSON.stringify({boost_id: id})
-    }).then(response => {
-        if (response.ok) {
-            return response.json()
-        } else {
-            return Promise.reject(response)
-        }
-    }).then(data => {
-        document.querySelector('.clickPower').innerHTML = textClickPower + data['clickPower'];
-        document.querySelector('.coins').innerHTML = textCoins + data['coins'];
-        document.querySelector('.price-' + id).innerHTML = data['price'] + textPrice;
+    cache.then(cacheData => {
+        let token = getCookie('csrftoken')
+        let coins = cacheData['coins']
+
+        saveCoins(coins).then(_ => {
+            fetch('/clicker/buyBoost/', {
+                method: 'POST',
+                headers: {"X-CSRFToken": token, 'Content-Type': 'application/json'},
+                body: JSON.stringify({boost_id: id})
+            }).then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    return Promise.reject(response)
+                }
+            }).then(data => {
+                cacheData['clickPower'] = data['clickPower']
+                cacheData['autoClickPower'] = data['autoClickPower']
+                cacheData['coins'] = data['coins']
+
+                document.querySelector('.clickPower .data').innerText = data['clickPower'];
+                document.querySelector('.autoClick .data').innerText = data['autoClickPower'];
+                document.querySelector('.coins .data').innerText = data['coins'];
+                document.querySelector('.price-' + id).innerHTML = data['price'];
+
+                activateBoosts()
+            })
+        })
     })
 }
 
@@ -76,23 +92,101 @@ function getCookie(name) {
 
 function renderBoost(boost) {
     let boostList = document.querySelector('.boosts')
+
+    if (boost['type'] === 1)
+        boostList = document.querySelector('.autoClickBoosts')
+
     let boostTemplate = document.querySelector('#boost-template').content;
     let newBoost = boostTemplate.querySelector('.boost')
-
     let boostItem = newBoost.cloneNode(true)
     let text = boostItem.querySelector('.text-simp')
-    text.textContent = boost.name + " (+" + boost.power + " power)"
+    text.textContent = boost.name + " (+" + boost['power'] + " power)"
 
     let price = boostItem.querySelector('.simp-price')
-    price.textContent = boost.price + " coins"
+    price.textContent = boost['price']
     price.classList.add('price-' + boost.id)
 
     let image = boostItem.querySelector('.image-simp')
-    image.src = '/static/hentai_information/images/simp-1.png'
+
+    if (boost['type'] === 0)
+        image.src = '/static/hentai_information/images/simp-1.png'
+    if (boost['type'] === 1)
+        image.src = '/static/hentai_information/images/autoBoost-1.png'
 
     boostItem.addEventListener('click', function () {
         buyBoost(boost.id, 1)
     })
 
     boostList.appendChild(boostItem)
+}
+
+function setAutoClickInterval() {
+    setInterval(function () {
+        cache.then(data => {
+            let coinsField = document.querySelector('.coins .data')
+            let autoClickField = document.querySelector('.autoClick .data').innerText
+
+            data['coins'] += parseInt(autoClickField)
+            coinsField.innerText = data['coins'];
+
+            activateBoosts()
+        })
+    }, 1000)
+}
+
+function saveCoinsInterval() {
+    setInterval(async function () {
+        cache.then(cacheData => {
+            saveCoins(cacheData['coins'])
+        })
+    }, 10000)
+}
+
+function saveCoins(coins) {
+    let token = getCookie('csrftoken')
+
+    return fetch('/clicker/saveCoins/', {
+        method: 'POST',
+        headers: {
+            "X-CSRFToken": token,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            coins: coins,
+        })
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject(response)
+        }
+    }).then(data => {
+        let boost = data['boost']
+
+        if (boost)
+            renderBoost(boost)
+    }).catch(err => console.log(err))
+}
+
+function activateBoosts() {
+    cache.then(data => {
+        let coins = data['coins']
+        const boosts = document.querySelectorAll('.boost')
+
+        for (let boost of boosts) {
+            activateBoost(coins, boost)
+        }
+    })
+}
+
+function activateBoost(coins, boost) {
+    let price = parseInt(boost.querySelector(".simp-price").innerText)
+
+    if (price > coins) {
+        boost.setAttribute('disabled', 'true')
+        boost.style.pointerEvents = 'none';
+    } else {
+        boost.removeAttribute('disabled')
+        boost.style.pointerEvents = 'auto';
+    }
 }
